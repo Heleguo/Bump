024da28c067d064a0b7ca256b739378e8a5809d9
mclk623 <mclk623@163.com>
2025-08-23 16:51:01 +0800

    fix

--- src/main/java/io/github/slimefunguguproject/bump/api/appraise/AppraiseResult.java
+++ src/main/java/io/github/slimefunguguproject/bump/api/appraise/AppraiseResult.java
@@ -14,6 +14,7 @@ import org.bukkit.Material;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.EquipmentSlotGroup;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
